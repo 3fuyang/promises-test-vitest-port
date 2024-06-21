43c93217207c0dfb6f1f420748e0dbf137c8c0d3
fwio <624835339@qq.com>
2024-06-21 23:29:31 +0800

    fix: unhandled rejection in 2.3.3

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier"
+import jsdoc from "eslint-plugin-jsdoc"
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  jsdoc.configs["flat/recommended"],
   {
     rules: {
       "prefer-arrow-callback": "error",
       "object-shorthand": "error",
+      "jsdoc/require-jsdoc": "off",
     },
   },
 ]
